f2bd305677383bbecda06fa3e665e8ca93910b39
browna38 <ajbrown000@gmail.com>
2020-10-29 13:00:41 -0500

    added database

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,6 +13,17 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.set("useCreateIndex", true);
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
+});
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
