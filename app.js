17e3d97b00936cca1ab1bc60880d8228541a2da4
browna38 <ajbrown000@gmail.com>
2020-11-03 15:24:32 -0600

    added proxy server

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+const { createProxyMiddleware }  = require('http-proxy-middleware');
+
+const { routes } = require('./config.json');
+
+
 
 const app = express();
 
@@ -32,12 +37,12 @@ res.render("login");
 });
 
 app.get("/register", function(req, res){
-    req("http://0.0.0.0:3000");
+    res.render("stream");
   });
 
   app.post("/register", function(req, res){
 
-    res.render("stream")
+    res.render("stream");
 
     // background-image: url(data:image/svg+xml;charset=utf-8,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.or…2-.9%202-2V8l-6-6H6zm7%207V3.5L18.5%209H13z%22%2F%3E%3C%2Fg%3E%3C%2Fsvg%3E), 
     // url(chrome://resources/images/arrow_down.svg);
@@ -57,6 +62,17 @@ app.get("/register", function(req, res){
     //});
   
   });
+
+  for (route of routes) {
+    app.use(route.route,
+      createProxyMiddleware({
+            target: route.address,
+            pathRewrite: (path, req) => {
+                return path.split('/').slice(2).join('/'); // Could use replace, but take care of the leading '/'
+            }
+        })
+    );
+}
 
 app.listen(80, function() {
     console.log("Server started on port 80.");
